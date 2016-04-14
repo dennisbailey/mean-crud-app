b35886666e0e4c96392c5b5728af73fbad7fd43f
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-14 13:48:41 -0600

    Added password hashing and salting

--- src/server/models/users.js
+++ src/server/models/users.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
 var Schema = mongoose.Schema;
 
+var config = require('../../../_config');
+
 var UserSchema = new Schema({
   email: {
                type: String,
@@ -20,7 +22,32 @@ var UserSchema = new Schema({
 });
 
 // Hash the password before saving it to the DB
-
+UserSchema.pre('save', function(next) {
+  
+  var user = this;
+  
+  // check if the password is new or being modified
+  if (!user.isModified('password') {
+    return next();
+  };
+  
+  // generate a salt
+  bcrypt.genSalt(config.SALT_WORK_FACTOR, function(err, salt) {
+    if (err) { return next(err); }
+    
+    // hash that password
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) { return next(err); }
+      
+      // replace the plain text passworkd with the hashed and salted password
+      user.password = hash;
+      next();
+      
+    });
+    
+  });  
+    
+});
 
 // Password comparison and verification
 
