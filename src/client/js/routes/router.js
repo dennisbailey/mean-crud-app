948cab7af47517c09fee96758adafe99a6bddd01
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-15 11:47:07 -0600

    adding an auth service

--- src/client/js/routes/router.js
+++ src/client/js/routes/router.js
@@ -1,17 +1,17 @@
 app.config(function($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'js/templates/main.html',
+        templateUrl: 'templates/main.html',
         controller: 'myController'
       })
       
       .when('/register', {
-        templateUrl: 'js/templates/register.html',
+        templateUrl: 'templates/register.html',
         controller: 'registerController'
       })
       
       .when('/login', {
-        templateUrl: 'js/templates/login.html',
+        templateUrl: 'templates/login.html',
         controller: 'loginController'
       })
       
--- src/client/js/services.js
+++ src/client/js/services.js
@@ -1,55 +1,92 @@
 app.service('studentDataService', ['crudService', function(crudService) {
-  
-  
+
+
   return {
-    
+
     getAllStudents : function () {
       return crudService.getAll('students')
-      .then( function (result) { 
-                                 return result.data.data });
+      .then( function (result) {
+                                 return result.data.data; });
     },
-    
+
     addStudent: function(payload) {
       crudService.addOne('students', payload)
-      .then( function (result) { return result })
+      .then( function (result) { return result; })
       .catch( function (error) { return error; });
     },
-    
+
     deleteStudent : function(id) {
       crudService.deleteOne('students', id)
-      .then( function (result) { console.log(result); 
-                                 return result })
+      .then( function (result) { return result; })
       .catch( function (error) { return error; });
     }
-  
-  }
-  
+
+  };
+
 }]);
 
 app.service('crudService', ['$http', function($http) {
-  
+
   var data = [];
+
+  return {
+
+    getAll: function(resource) {
+              return $http.get('/' + resource)
+              .then( function (response) { return response; })
+              .catch( function (error) { return error; });
+            },
+
+    addOne: function(resource, payload) {
+              return $http.post('/' + resource, payload)
+              .then( function (response) { return response; })
+              .catch( function (error) { return error; });
+            },
+
+    deleteOne: function(resource, id) {
+                 return $http.delete(resource + '/delete/' + id)
+                 .then( function (response) { return response; })
+                 .catch( function (error) { return error; });
+               },
+
+  };
+
+}]);
+
+// Login
+// Register
+// Local Storage : Set User Info
+// Local Storage : Get User Info
+
+app.service('authService', ['$http', '$window', function($http, $window) {
+  
+  var user = {};
   
   return {
+
+    register: function(user) {
+                return $http.post('/auth/register', user);
+              },
+           
+    login: function(user) {
+             return $http.post('/auth/login', user);
+           },
+           
+    logout: function(user) {
+              user = null;
+              $window.localStorage.clear();
+            },
     
-    getAll: function(resource) {
-      return $http.get('/' + resource)
-      .then( function (response) { return response })
-      .catch( function (error) { return error; });    
-    },
-    
-    addOne: function(resource, payload) {
-      return $http.post('/' + resource, payload)
-      .then( function (response) { return response })
-      .catch( function (error) { return error; });
-    },
-    
-    deleteOne: function(resource, id) {
-      return $http.delete(resource + '/delete/' + id)
-      .then( function (response) { return response })
-      .catch( function (error) { return error; });
-    },
-    
-  }
+    setUserInfo: function(userData) {
+                   $window.localStorage.set('user', 'PLACEHOLDER');
+                   $window.localStorage.set('token', 'PLACEHOLDER');
+                 },
+                 
+   getUserInfo: function(userData) {
+                   $window.localStorage.get('user', 'PLACEHOLDER');
+                   $window.localStorage.get('token', 'PLACEHOLDER');
+                 },              
   
-}])
+  };
+
+}]);
